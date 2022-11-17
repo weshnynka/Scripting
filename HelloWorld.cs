b64a9d4057eb024b0d79ca425458114e62398fed
weshnynka <118459225+weshnynka@users.noreply.github.com>
2022-11-17 19:57:04 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод в консоль приветствия миру
+        print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
